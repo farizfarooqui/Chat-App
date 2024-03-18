0ed0fa94021c921c997df071afdfe17aea683845
MUHAMMAD FARIZ FAROOQUI <135133746+farizfarooqui@users.noreply.github.com>
2024-03-18 23:13:38 +0500

    char service

---
+++ lib/UI/Authentication Screens/widgets/user_tile.dart
@@ -0,0 +1,15 @@
+// import 'package:flutter/material.dart';
+
+// class UserTileWidget extends StatelessWidget {
+//   final String text;
+//   final void Function()? ontap;
+//   const UserTileWidget({super.key, required this.text, this.ontap});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListTile(
+//       style: BorderRadius.circular(30),
+//       leading: Icon(Icons.person),
+//     );
+//   }
+// }
--- lib/UI/Home Screen/home_screen.dart
+++ lib/UI/Home Screen/home_screen.dart
@@ -1,60 +1,62 @@
-// ignore: unnecessary_import
-// import 'dart:js';
-import 'dart:js';
-import 'dart:ui';
+import 'package:flutter/material.dart';
 import 'package:chatapp/Service/Auth%20Service/auth_service.dart';
 import 'package:chatapp/Service/Chat%20service/char_service.dart';
 import 'package:chatapp/UI/Home%20Screen/Widget/drawer_widget.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
+  HomeScreen({Key? key});
 
-  //get chat service  instance
   final ChatService _chatService = ChatService();
   final AuthService _authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Hey Fariz',
-            style: TextStyle(color: Colors.blue),
-          ),
-          // systemOverlayStyle: const SystemUiOverlayStyle(
-          //     statusBarIconBrightness: Brightness.light,
-          //     statusBarColor: Colors.transparent),
-          // backgroundColor: Colors.transparent,
-          // shadowColor: Colors.transparent,
+      appBar: AppBar(
+        title: const Text(
+          'Hey Fariz',
+          style: TextStyle(color: Colors.blue),
         ),
-        drawer: const DrawerWidget(),
-        body: const Center(child: Text('Hello Fariz keep the hardwork')));
+      ),
+      drawer: const DrawerWidget(),
+      body: _buildUserList(),
+    );
   }
 
-  // Widget _buildUserList() {
-  //   return StreamBuilder(
-  //       stream: _chatService.getUserStream(),
-  //       builder: (context, snapshot) {
-  //         if (snapshot.hasError) {
-  //           return Text('data');
-  //         } else if (snapshot.connectionState == ConnectionState.waiting) {
-  //           return CircularProgressIndicator();
-  //         } else {
-  //           return ListView(
-  //             children: snapshot.data!
-  //                 .map<Widget>((userData) => _buildUserListItem)
-  //                 .toList(),
-  //           );
-  //         }
-  //       });
-  // }
+  Widget _buildUserList() {
+    return StreamBuilder(
+      stream: _chatService.getUserStream(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else {
+          return ListView(
+            children: snapshot.data!
+                .map<Widget>((userData) => _buildUserListItem(userData, context))
+                .toList(),
+          );
+        }
+      },
+    );
+  }
 
-  // //build indivitual user list tile
-  // Widget _buildUserListItem(
-  //     Map<String, dynamic> userData, BuildContext context) {
-  //   //display all users except current user
-  //   return ListTile();
-  // }
+  Widget _buildUserListItem(Map<String, dynamic> userData, BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15), 
+      ),
+      child: ListTile(
+        onTap: () {
+        },
+        leading: CircleAvatar(
+          backgroundColor: Colors.blue,
+          child: Icon(Icons.person),
+        ),
+        title: Text(userData['name']),
+        subtitle: Text(userData['email']),
+      ),
+    );
+  }
 }
